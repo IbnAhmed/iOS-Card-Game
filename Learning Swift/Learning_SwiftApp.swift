ccee04c0abd20966990fcfe7306f21c8e670641f
Muntaha Ibn Ahmed <ahamedmuntaha@gmail.com>
2022-03-23 08:24:27 +0600

    Initial Commit

---
+++ Learning Swift/Learning_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftApp.swift
+//  Learning Swift
+//
+//  Created by Muntaha Ibn Ahmed on 20/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
